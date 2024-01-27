3522f06c58c88fc76b7deb6a8c3410c80e28d616
RedEyesBadDragon <106855213+RedEyesBadDragon@users.noreply.github.com>
2024-01-27 05:00:37 -0800

    V2 Dev12.5 BREAKTHROUGH?!?!
    I think we are now observing the first SCALAR excitation being harvested by the addition of output coils shifted 90 degrees as Lt Bearden had in the MEG - this is most likely due to it being seen as a changing magnetic field by the output coil therefor when we measure the output coil we are seeing a perfectly induced 1 to 1 electric wave being picked up
    
    We can harvest energy form this output coil!!!
    
    captures were done and added to the git
    
    I will be updated the Dev journal Monday along with going into more tests / self powering with the additional output coils
    
    - apologies for not updating the Dev journal now. I am tired x.x

---
+++ version_02 - (IN PROGRESS)/code/v2TestCode/v2TestCode.ino
@@ -0,0 +1,63 @@
+/*
+This program is an open source waveform generator for the
+Arduinno platform of microcontrollers.
+-
+We are using it to drive a carrier wave for and Over-Unity
+Electromagnetic Field transformer (i.e. an "infinite energy device").
+-
+All the information for how and why such devices are possible can 
+be found at:
+https://www.youtube.com/@RedEyesBadDragon
+https://www.siriusdisclosure.com
+-
+As of now all these things are being developed LIVE 
+via: https://www.twitch.tv/redeyesbaddragon
+
+Written By: V Drakaina
+Start Date: 01/27/2024
+End Date: TBD
+*/
+
+
+#define CARRIER_WAVE 5
+
+void setup() {
+  Serial.begin(9600);   //begin the serial w/ baud rate
+
+  pinMode(CARRIER_WAVE, OUTPUT);  
+  digitalWrite(CARRIER_WAVE, LOW);
+}
+
+void loop() {
+  
+  Serial.println("Wave Form Begin");
+  
+  //Squares Basic
+  Serial.println("YOU ARE GENERATING SQUARES");
+  squares();
+}
+
+//-----------------------------------------------------------------------//
+//FUNCTION: squares
+//INPUTS: void
+//OUTPUTS: void
+//
+//Details: This function outputs a variable duty cycle and frequency 
+//square wave where the MAX frequency/voltage peak achievable is determined by the 
+//Arduino's clock speed/operating voltage.  
+//-----------------------------------------------------------------------//
+void squares() {
+
+  int t = 5;    //Delay time between on and off square waves (us)
+  //int t0 = 0;   //Delay time after ON signal for square wave (us)
+  //int t1 = 0;   //Delay time after OFF signal for square wave (us)  
+
+  while(1) { 
+    digitalWrite(CARRIER_WAVE, HIGH);
+    delayMicroseconds(t);
+    digitalWrite(CARRIER_WAVE, LOW);
+    delayMicroseconds(t);
+  }
+
+}
+//-----------------------------------------------------------------------//
